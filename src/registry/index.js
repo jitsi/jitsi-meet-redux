854f20ba68367b9dad81aefb975edc79c32bc7d6
Bettenbuk Zoltan <zoltan.bettenbuk@gmail.com>
2019-09-24 09:16:10 +0200

    feat: factory functions for non singleton use

--- src/registry/index.js
+++ src/registry/index.js
@@ -1,2 +1,4 @@
 export { default as MiddlewareRegistry } from './MiddlewareRegistry';
+export { MiddlewareRegistry as _MiddlewareRegistry } from './MiddlewareRegistry';
 export { default as ReducerRegistry } from './ReducerRegistry';
+export { ReducerRegistry as _ReducerRegistry } from './ReducerRegistry';
